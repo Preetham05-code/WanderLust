37d244bacec85bb98dd9bc5c5402eafe8cffdd77
Preetham Gowda M <preethamgowdam18@gmail.com>
2026-09-20 21:56:12 +0530

    Configure Firebase Admin for Render

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,7 +1,19 @@
-const { initializeApp, applicationDefault } = require("firebase-admin/app");
+const { initializeApp, cert, applicationDefault } = require("firebase-admin/app");
 
-const admin = initializeApp({
-    credential: applicationDefault()
-});
+let app;
 
-module.exports = admin;
+if (process.env.FIREBASE_PRIVATE_KEY) {
+    app = initializeApp({
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        })
+    });
+} else {
+    app = initializeApp({
+        credential: applicationDefault()
+    });
+}
+
+module.exports = app;
